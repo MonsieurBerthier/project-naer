0bac86f963a07189242260a49e9cb35937ebd037
MonsieurBerthier <jeanchristian.berthier@gmail.com>
2023-08-21 22:56:45 +0200

    Setting all attributes from private to public

--- ground.py
+++ ground.py
@@ -8,23 +8,19 @@ class Ground:
 
     def __init__(self, main, ground: str) -> None:
 
-        self._main = main
-        self._ground_name = ground
+        self.main = main
+        self.name = ground
 
-        self._ground_path = os.path.join(self._main.PATH_GROUNDS, self._ground_name)
-        self._ground_model = None
+        self.path = os.path.join(self.main.PATH_GROUNDS, self.name)
+        self.model = None
 
         self.load_ground()
 
     def load_ground(self) -> None:
 
-        logger.debug(f"Loading ground \"{self._ground_name}\"")
+        logger.debug(f"Loading ground \"{self.name}\"")
 
-        ground_glb = library.io.get_file_path(path=self._ground_path, extension="glb")
-        self._ground_model = self._main.loader.loadModel(modelPath=os.path.join(self._ground_path, ground_glb))
-        self._ground_model.setLightOff()
-        self._ground_model.reparentTo(self._main.render)
-
-    def get_ground_model(self):
-
-        return self._ground_model
+        glb = library.io.get_file_path(path=self.path, extension="glb")
+        self.model = self.main.loader.loadModel(modelPath=os.path.join(self.path, glb))
+        self.model.setLightOff()
+        self.model.reparentTo(self.main.render)
