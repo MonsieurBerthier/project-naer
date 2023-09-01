ec6a9d80a944b18456e008fd07d726662d207bfd
MonsieurBerthier <jeanchristian.berthier@gmail.com>
2023-09-01 22:47:51 +0200

    Implementation of oem wheels loading

--- car.py
+++ car.py
@@ -9,7 +9,7 @@ class Car:
     def __init__(self, main) -> None:
 
         self.main = main
-        self.name = self.main.config_json["default"]["car"]
+        self.name = self.main.config_json["defaults"]["car"]
 
         self.path = None
         self.json = None
@@ -28,35 +28,19 @@ class Car:
         self.json = library.io.get_json(path=os.path.join(self.path, self.main.PATH_ITEMS_CONFIG_JSON))
 
         self.models = {"chassis": self.main.loader.loadModel(modelPath=os.path.join(self.path,
-                                                                                    self.main.PATH_CARS_CHASSIS))}
+                                                                                    self.main.PATH_CARS_CHASSIS)),
+                       "wheels": []}
         self.models["chassis"].reparentTo(self.main.render)
         self.models["chassis"].setPos(tuple(self.json["chassis"]["position"]))
         self.models["chassis"].setHpr(tuple(self.json["chassis"]["rotation"]))
         self.models["chassis"].setScale(tuple(self.json["chassis"]["scale"]))
 
-        # FIXME Use default wheels provided with the car instead
-        wheels_json = library.io.get_json(path="content/wheels/japanracing_jr3/config.json")
-        for wheel in self.json["wheels"]:
-            logger.debug(f"Loading wheel \"{'japanracing_jr3'}\"")
-            wheel_x = self.main.loader.loadModel(modelPath="content/wheels/japanracing_jr3/model.glb")
-            wheel_x.setPos(tuple([a + b for a, b in zip(wheel["position"], wheels_json["position"])]))
-            wheel_x.setHpr(tuple([a + b for a, b in zip(wheel["rotation"], wheels_json["rotation"])]))
-            wheel_x.setScale(tuple([a * b for a, b in zip(wheel["scale"], wheels_json["scale"])]))
-            wheel_x.reparentTo(self.main.render)
+        for part in self.json["defaults"]:
 
-        for part in self.json["default"]:
-            self.load_part(part=part)
-
-    def unload(self):
-
-        logger.debug(f"Unloading car \"{self.name}\"")
-
-        self.path = None
-        self.json = None
-
-        for item in self.models:
-            self.models[item].removeNode()
-        self.models = None
+            if "wheel" in part:
+                self.load_wheels(name=part, oem=True)
+            else:
+                self.load_part(part=part)
 
     def load_part(self, part: str) -> None:
 
@@ -72,3 +56,55 @@ class Car:
         self.models[part_type].setHpr(tuple(self.json["chassis"]["rotation"]))
         self.models[part_type].setScale(tuple(self.json["chassis"]["scale"]))
         self.models[part_type].reparentTo(self.main.render)
+
+    def load_wheels(self, name: str, oem: bool) -> None:
+
+        if self.models["wheels"]:
+            self.unload_wheels()
+
+        logger.debug(f"Loading car wheel \"{name}\"")
+
+        if oem:
+            wheel_path = os.path.join(self.path, name + ".glb")
+        else:
+            wheel_path = os.path.join(self.main.PATH_WHEELS, name, name + ".glb")
+
+        for wheel in self.json["wheels"]:
+            wheel_model = self.main.loader.loadModel(modelPath=wheel_path)
+            wheel_model.setPos(tuple(wheel["position"]))
+            wheel_model.setHpr(tuple(wheel["rotation"]))
+            wheel_model.setScale(tuple(wheel["scale"]))
+            wheel_model.reparentTo(self.main.render)
+            self.models["wheels"].append(wheel_model)
+
+    def unload(self):
+
+        logger.debug(f"Unloading car \"{self.name}\"")
+
+        self.path = None
+        self.json = None
+
+        for item in self.models:
+            if isinstance(self.models[item], list):
+                self.unload_wheels()
+            else:
+                self.unload_part(part=item)
+
+        self.models = None
+
+    def unload_part(self, part: str) -> None:
+
+        logger.debug(f"Unloading car part \"{part}\"")
+
+        self.models[part].removeNode()
+
+        self.models.pop("part", None)
+
+    def unload_wheels(self) -> None:
+
+        logger.debug(f"Unloading car wheels")
+
+        for wheel in self.models["wheels"]:
+            wheel.removeNode()
+
+        self.models["wheels"] = []
