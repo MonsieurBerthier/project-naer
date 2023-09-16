48d272f8490397fad569b44934b998e81799415f
MonsieurBerthier <jeanchristian.berthier@gmail.com>
2023-09-16 15:59:01 +0200

    Update of get_file_path() function

--- library/io.py
+++ library/io.py
@@ -26,8 +26,8 @@ def get_file_path(path: str, extension: str, number: int = 1) -> Union[list, str
     :type path: ``str``
     :param extension: extension of files to be returned
     :type extension: ``str``
-    :param number: number of files to be returned
-    :type number: ``str``
+    :param number: number of files to be returned, passing "0" will return all found files
+    :type number: ``int``
 
     :return: a list of filenames or a filename if ``number`` equals 1
     :rtype: ``list[str]`` or ``str``
@@ -35,7 +35,9 @@ def get_file_path(path: str, extension: str, number: int = 1) -> Union[list, str
 
     list_of_files = [f for f in os.listdir(path) if f.endswith(extension)]
 
-    if number == 1:
+    if number == 0:
+        return list_of_files
+    elif number == 1:
         return list_of_files[0]
     else:
         return list_of_files[:number]
